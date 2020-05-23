2c989fa60076fe057e4f626283cf0f5354ec95c6
Хумарь <piplap62@gmail.com>
2020-05-23 09:09:13 +0300

    Add package and app

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const userRouter = require("./routes/userRouter.js");
+const PORT = process.env.PORT || 5000;
+const path = require('path');
+
+const jsonParser = express.json();
+
+app.use(jsonParser);
+app.use("/users", userRouter);
+
+app.use(function (req, res) {
+  res.status(404).send("Not Found");
+});
+
+app.use(function(err, req, res,) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build/index.html'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(PORT);
