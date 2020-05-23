44579542ca9ddbe05306cbd3966d76e7427b523c
Хумарь <piplap62@gmail.com>
2020-05-23 12:23:43 +0300

    delete homepage

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use("/users", userRouter);
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build/index.html'));
 
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
